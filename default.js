6b77cd6e38195838dab25bb772513bf324fa196a
wadaarata <wada.arata10@gmail.com>
2018-12-28 01:22:04 +0900

    add default.js

---
+++ default.js
@@ -0,0 +1,13 @@
+let box1 = document.querySelector('.box1');
+let box2 = document.querySelector('.box2');
+let box3 = document.querySelector('.box2_3');
+
+if (box1 != null) {
+    console.log(box1);
+}
+if (box2 != null) {
+    console.log(box2);
+}
+if (box3 != null) {
+    console.log(box3);
+}

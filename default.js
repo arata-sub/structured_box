868cd9119ca6642d9804e782fa39c37e2212d462
wadaarata <wada.arata10@gmail.com>
2019-01-03 02:27:03 +0900

    add js function,automation box-width

--- default.js
+++ default.js
@@ -1,13 +1,15 @@
-let box1 = document.querySelector('.box1');
-let box2 = document.querySelector('.box2');
-let box3 = document.querySelector('.box2_3');
+let box2Children = document.querySelector('.boxA').children;
+let box2Count = document.querySelector('.boxA').childElementCount;
+console.log(box2Children.length);
 
-if (box1 != null) {
-    console.log(box1);
+function widthSelector(box2Count) {
+    let width = 100 / box2Count;
+    console.log(width + '%');
+    return width + '%';
 }
-if (box2 != null) {
-    console.log(box2);
+
+for (let i = 0; i < box2Count; i++) {
+   box2Children[i].style.width = widthSelector(box2Count);
+   box2Children[i].style.float = 'left';
 }
-if (box3 != null) {
-    console.log(box3);
-}
+
